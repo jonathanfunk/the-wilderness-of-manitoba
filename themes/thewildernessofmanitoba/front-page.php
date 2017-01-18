334f302980718e986d29b3e62f2532fb245cc394
Jonathan Funk <jonfunk88@hotmail.com>
2017-01-17 22:35:39 -0800

    styled title bars

--- themes/thewildernessofmanitoba/front-page.php
+++ themes/thewildernessofmanitoba/front-page.php
@@ -1,19 +1,61 @@
 <?php
 /**
- * The main template file.
- *
- * @package RED_Starter_Theme
- */
+* The main template file.
+*
+* @package RED_Starter_Theme
+*/
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-      <div class="collumn">
-      </div>
-			<div class="collumn">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<div class="collumn"><!-- Column 1 -->
+			<div class="panel"><!-- Wilderness of Manitoba -->
+				<h3>The Wilderness of Manitoba</h3>
+				<div class="content">
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+				</div>
 			</div>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			<div class="panel"><!-- Videos -->
+				<div class="title">
+					<h3>Videos</h3>
+				</div>
+				<div class="content">
+				</div>
+			</div>
+		</div>
+		<div class="collumn"><!-- Collumn 2 -->
+			<div class="panel"><!-- Music -->
+				<div class="title">
+					<h3>Music</h3>
+				</div>
+				<div class="content">
+				</div>
+			</div>
+			<div class="panel"><!-- Press -->
+				<div class="title">
+					<h3>Press</h3>
+				</div>
+				<div class="content">
+				</div>
+			</div>
+			<div class="panel"><!-- Highlights -->
+				<div class="title">
+					<h3>Highlights</h3>
+				</div>
+				<div class="content">
+				</div>
+			</div>
+			<div class="panel"><!-- Photos -->
+				<div class="title">
+					<h3>Photos</h3>
+				</div>
+				<div class="content">
+				</div>
+			</div>
+		</div>
+	</main><!-- #main -->
+</div><!-- #primary -->
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- themes/thewildernessofmanitoba/sidebar.php
+++ themes/thewildernessofmanitoba/sidebar.php
@@ -11,6 +11,19 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <div id="secondary" class="widget-area" role="complementary">
-	<p>Sidey</p>
+	<div class="panel"><!-- Discography -->
+		<div class="title">
+			<h3>Discography</h3>
+		</div>
+		<div class="content">
+		</div>
+	</div>
+	<div class="panel"><!-- Contact -->
+		<div class="title">
+			<h3>Contact</h3>
+		</div>
+		<div class="content">
+		</div>
+	</div>
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </div><!-- #secondary -->
