055053c2549b7d07b775ab92ad9eff6ccbe5e488
Jonathan Funk <jonfunk88@hotmail.com>
2017-01-18 18:40:29 -0800

    media sizes added

--- themes/thewildernessofmanitoba/front-page.php
+++ themes/thewildernessofmanitoba/front-page.php
@@ -31,6 +31,8 @@ get_header(); ?>
 					<h3>Music</h3>
 				</div>
 				<div class="content">
+
+<iframe width="100%" height="260" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/289580674%3Fsecret_token%3Ds-Jwjio&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
 				</div>
 			</div>
 			<div class="panel"><!-- Press -->
