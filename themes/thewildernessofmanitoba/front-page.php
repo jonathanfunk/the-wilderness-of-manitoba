52867cce57ef9a1917852d17d3a8ce8fa81f73ec
Jonathan Funk <jonfunk88@hotmail.com>
2017-01-21 14:04:10 -0800

    site is now responsive on all browsers

--- themes/thewildernessofmanitoba/front-page.php
+++ themes/thewildernessofmanitoba/front-page.php
@@ -7,71 +7,69 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
-		<div class="collumn"><!-- Column 1 -->
-			<h3>The<br><!-- Wilderness of Manitoba -->
-				Wilderness<br>
-				of Manitoba</h3>
-				<div class="content">
-					<p>prefers to characterize itself as an evolving entity and founding member Will Whitwham is the last man standing.</p>
-					<p>For the band’s new album, Across The Dark, the vocalist/multi-instrumentalist stepped up to write and produce all the songs himself. “When a band isn’t changing, it’s not growing,” says Whitwham. From the band’s inception, it has redefined itself with every new release; from Hymns of Love and Spirits through When You Left The Fire, Island of Echoes, Between Colours and various EPs. </p>
-					<p>The evolution of their sound has run the gamut from four part harmonies and its initial chamber folk sound to duo lead vocals and an expanded sonic palette ranging from cinematic indie pop to anthemic folk rock. The current band includes vocalist/guitarist Raven Shields, bassist/vocalist Tavo Diez de Bonilla and drummer Mike Brushey.</p>
-					<p>In its eight year existence, the Wilderness of Manitoba’s strengths come as a direct result of the wealth of traveling the band has done; playing a multitude of festivals and touring in the UK, the Netherlands, Germany, the USA and Canada. “The live show,” says Whitwham, “is an in-the-moment portrait of the band and its efforts.” Those efforts were recognized when the Wilderness of Manitoba was nominated for a JUNO, the Canadian equivalent to a Grammy or Brit Award. Summing things up, Whitwham says, “There is no lacking in the quantity of ideas in terms of our music and the band’s future.” </p>
-				</div>
-				<div class="panel"><!-- Videos -->
-					<div class="title">
-						<h3>Videos</h3>
-					</div>
-					<div class="content">
-						<iframe width="100%" height="170" src="https://www.youtube.com/embed/CAGloL6ySOk" frameborder="0" allowfullscreen></iframe>
-						<iframe width="100%" height="170" src="https://www.youtube.com/embed/Qtz6KM3HDIg" frameborder="0" allowfullscreen></iframe>
-						<iframe width="100%" height="170" src="https://www.youtube.com/embed/Zm8AqyQf9Gk" frameborder="0" allowfullscreen></iframe>
-					</div>
-				</div>
+<main id="main" class="site-main" role="main">
+	<div class="collumn"><!-- Column 1 -->
+		<h3>The<br><!-- Wilderness of Manitoba -->
+			Wilderness<br>
+			of Manitoba</h3>
+			<div class="content">
+				<p>prefers to characterize itself as an evolving entity and founding member Will Whitwham is the last man standing.</p>
+				<p>For the band’s new album, Across The Dark, the vocalist/multi-instrumentalist stepped up to write and produce all the songs himself. “When a band isn’t changing, it’s not growing,” says Whitwham. From the band’s inception, it has redefined itself with every new release; from Hymns of Love and Spirits through When You Left The Fire, Island of Echoes, Between Colours and various EPs. </p>
+				<p>The evolution of their sound has run the gamut from four part harmonies and its initial chamber folk sound to duo lead vocals and an expanded sonic palette ranging from cinematic indie pop to anthemic folk rock. The current band includes vocalist/guitarist Raven Shields, bassist/vocalist Tavo Diez de Bonilla and drummer Mike Brushey.</p>
+				<p>In its eight year existence, the Wilderness of Manitoba’s strengths come as a direct result of the wealth of traveling the band has done; playing a multitude of festivals and touring in the UK, the Netherlands, Germany, the USA and Canada. “The live show,” says Whitwham, “is an in-the-moment portrait of the band and its efforts.” Those efforts were recognized when the Wilderness of Manitoba was nominated for a JUNO, the Canadian equivalent to a Grammy or Brit Award. Summing things up, Whitwham says, “There is no lacking in the quantity of ideas in terms of our music and the band’s future.” </p>
 			</div>
-			<div class="collumn"><!-- Collumn 2 -->
-				<div class="title"><!-- Music -->
-					<h3>Music</h3>
-				</div>
-				<div class="content">
-					<iframe width="100%" height="260" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/289580674%3Fsecret_token%3Ds-Jwjio&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
-				</div>
-			<div class="panel"><!-- Press -->
+			<div class="panel"><!-- Videos -->
 				<div class="title">
-					<h3>Press</h3>
+					<h3>Videos</h3>
 				</div>
 				<div class="content">
-				</div>
-			</div>
-			<div class="panel"><!-- Highlights -->
-				<div class="title">
-					<h3>Highlights</h3>
-				</div>
-				<div class="content">
-				</div>
-			</div>
-			<div class="panel"><!-- Photos -->
-				<div class="title">
-					<h3>Photos</h3>
-				</div>
-				<div class="content">
+					<iframe width="100%" height="170" src="https://www.youtube.com/embed/CAGloL6ySOk" frameborder="0" allowfullscreen></iframe>
+					<iframe width="100%" height="170" src="https://www.youtube.com/embed/Qtz6KM3HDIg" frameborder="0" allowfullscreen></iframe>
+					<iframe width="100%" height="170" src="https://www.youtube.com/embed/Zm8AqyQf9Gk" frameborder="0" allowfullscreen></iframe>
 				</div>
 			</div>
 		</div>
-		<aside>
-			<div class="title"><!-- Discography -->
-				<h3>Discography</h3>
+		<div class="collumn"><!-- Collumn 2 -->
+			<div class="title"><!-- Music -->
+				<h3>Music</h3>
+			</div>
+			<div class="content">
+				<iframe width="100%" height="260" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/289580674%3Fsecret_token%3Ds-Jwjio&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+			</div>
+		<div class="panel"><!-- Press -->
+			<div class="title">
+				<h3>Press</h3>
 			</div>
 			<div class="content">
 			</div>
-			<div class="title"><!-- Contact -->
-				<h3>Contact</h3>
+		</div>
+		<div class="panel"><!-- Highlights -->
+			<div class="title">
+				<h3>Highlights</h3>
 			</div>
 			<div class="content">
 			</div>
-		</aside>
-	</main><!-- #main -->
-</div><!-- #primary -->
+		</div>
+		<div class="panel"><!-- Photos -->
+			<div class="title">
+				<h3>Photos</h3>
+			</div>
+			<div class="content">
+			</div>
+		</div>
+	</div>
+	<aside>
+		<div class="title"><!-- Discography -->
+			<h3>Discography</h3>
+		</div>
+		<div class="content">
+		</div>
+		<div class="title"><!-- Contact -->
+			<h3>Contact</h3>
+		</div>
+		<div class="content">
+		</div>
+	</aside>
+</main><!-- #main -->
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
